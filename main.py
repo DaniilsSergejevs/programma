0edb2b07f84b164bfc07bdd9e673b16f45ee9149
Daniil Sergejev <77155713+DaniilsSergejevs@users.noreply.github.com>
2022-09-07 12:04:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+x = int (input("Ievadi abolu daudzumu: "))
+if x >= 10:
+  print("Wow! Tev ir daudz abolus!");
+elif x>0 and x<10:
+  print("Wow! Vai tiešām tev ar to pietiek?");
+else: (input(";("));
